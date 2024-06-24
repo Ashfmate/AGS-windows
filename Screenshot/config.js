321f11eaf95ebe777fa0745586990e4e70414201
Ashfmate <ashforestunknown@gmail.com>
2024-06-24 12:55:06 +0300

    Cleaned up switching of buttons

--- Screenshot/config.js
+++ Screenshot/config.js
@@ -1,75 +1,94 @@
 export { }
-import { icon_path, rad_btn_children, RadioButton, chooser } from './radio_buttons.js';
+App.addIcons(App.configDir + '/windows/Screenshot/Icons')
+class WidgetSwitcher {
+    /**
+     * @param {import('/home/ashforest/.config/ags/types/widgets/widget').Widget} widget
+     * @param {string} class_name
+     * */
+    constructor(widget, class_name) {
+        this.old_widget = { widget, class_name }
+        this.new_widget = Variable(this.old_widget)
+        this.new_widget.connect('changed', (self) => {
+            self.getValue().widget.toggleClassName(self.getValue().class_name, true)
+            if (self.getValue().widget != this.old_widget.widget) {
+                this.old_widget.widget.toggleClassName(this.old_widget.class_name, false)
+                this.old_widget = self.getValue()
+            }
+        })
+    }
 
-let file_name = ''
-
+    /**
+     * @param {import('/home/ashforest/.config/ags/types/widgets/widget').Widget} widget
+     * @param {string} class_name
+     * */
+    takeActive(widget, class_name) {
+        this.new_widget.setValue({
+            widget,
+            class_name
+        })
+    }
+}
 const file_input = Widget.Entry({
     placeholder_text: 'file_name',
     text: Utils.exec('date "+%Y_%m_%d-%H_%M_%S"') + '.png',
-    onAccept: ({ text }) => {
-        if (text) {
-            file_name = text
-        }
-    },
     class_name: 'file_input',
     xalign: 0.3,
 })
 
+
 const region_cap = Widget.Button({
-    child: Widget.Icon(icon_path('tool-crop')),
+    class_name: 'button',
+    attribute: () => {
+        Utils.execAsync('hyprshot -m region -f "' + file_input.text + '"')
+    },
+    child: Widget.Icon('tool-crop'),
+    on_clicked: (self) => {
+        ws.takeActive(self, 'button_active')
+    }
 })
 const win_cap = Widget.Button({
-    child: Widget.Icon(icon_path('window-alt')),
+    class_name: 'button',
+    attribute: () => {
+        Utils.execAsync('hyprshot -m window -f "' + file_input.text + '"')
+    },
+    child: Widget.Icon('window-alt'),
+    on_clicked: (self) => {
+        ws.takeActive(self, 'button_active')
+    }
 })
 const full_cap = Widget.Button({
-    child: Widget.Icon(icon_path('expand')),
+    class_name: 'button',
+    attribute: () => {
+        Utils.execAsync('hyprshot -m output -f "' + file_input.text + '"')
+    },
+    child: Widget.Icon('expand'),
+    on_clicked: (self) => {
+        ws.takeActive(self, 'button_active')
+    }
 })
-
-rad_btn_children.push(new RadioButton(region_cap, 'button_active', 'button_inactive'))
-rad_btn_children.push(new RadioButton(win_cap, 'button_active', 'button_inactive'))
-rad_btn_children.push(new RadioButton(full_cap, 'button_active', 'button_inactive'))
+const ws = new WidgetSwitcher(region_cap, 'button_active')
 
 const screen_cap = Widget.Box({
     class_name: 'screen_cap',
     hpack: "center",
     spacing: 8,
-    children: rad_btn_children.map(self => self.widget)
+    children: [region_cap, win_cap, full_cap]
 })
 
 const video = Widget.Button({
     class_name: 'media_btn',
-    child: Widget.Icon(icon_path('play-alt')),
+    child: Widget.Icon('play-alt(1)'),
     on_clicked: () => {
-        switch (chooser.value) {
-            case 0:
-                Utils.execAsync('hyprshot -m region -f "' + file_input.text + '"')
-                break
-            case 1:
-                Utils.execAsync('hyprshot -m window -f "' + file_input.text + '"')
-                break
-            case 2:
-                Utils.execAsync('hyprshot -m output -f "' + file_input.text + '"')
-                break
-        }
+        ws.new_widget.getValue().widget.attribute()
         App.quit()
     }
 })
 
 const picture = Widget.Button({
     class_name: 'media_btn',
-    child: Widget.Icon(icon_path('camera')),
+    child: Widget.Icon('camera(1)'),
     on_clicked: () => {
-        switch (chooser.value) {
-            case 0:
-                Utils.execAsync('hyprshot -m region -f "' + file_input.text + '"')
-                break
-            case 1:
-                Utils.execAsync('hyprshot -m window -f "' + file_input.text + '"')
-                break
-            case 2:
-                Utils.execAsync('hyprshot -m output -f "' + file_input.text + '"')
-                break
-        }
+        ws.new_widget.getValue().widget.attribute()
         App.quit()
     }
 })
@@ -91,7 +110,7 @@ const pop_up = Widget.CenterBox({
     end_widget: media_choice,
 })
 
-export const Bar = Widget.Window({
+export const ScreenshotPopUp = Widget.Window({
     monitor: 0,
     name: 'screenshot',
     exclusivity: 'exclusive',
